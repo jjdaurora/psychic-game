e77e6a72fc9992afc5b19b187f79b8233bb8e206
JJ D'Aurora <jjdaurora@gmail.com>
2017-11-03 18:40:53 -0400

    complete game - see issues board

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,55 +1,56 @@
-// //define the alphabet array, although I'm sure there is a better way to do this
-
-// var alphabet = ["a", "b", "c", "d", "e", "f", "g" ,"h", "i", "j", "k","l", "m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-
-
-// // define wins variable 
-
-
-// var userText = document.getElementById("user-wins");
-
-
-//   document.onkeyup = function(event) {
-//         userText.textContent = event.key;
-//       };
-
-//    	for (var i = 0; i < alphebet.length; i++) {
-//         console.log("I love " + vegetables[i]);
-//       }
+// here is the alphabet from which the computer will choose a random letter
 
 var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l',
     'm','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
-// Variables for tracking our wins, losses and ties. They begin at 0.
+// defining my variables for when the user wins, loses, the count of guesse per round, and the random letter genetor code
+
 var wins = 0;
 var losses = 0;
 var guesses = 10;
+var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+    console.log(computerChoice);
+
+// setting function for on key event; 
+// when the user presses a key, the following things will happen...
 
 document.onkeypress = function(event) {
 
-    var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
-
-    console.log(computerChoice);
-
-
-   // When the user presses a key, it will run the following function...
-
     var userGuess = event.key;
 
+// if the user user wins, increase wins by 1, retrigger another guess by the computer, and cler the guesses so far field
+    
     if (userGuess === computerChoice){
         wins++;
-    } else {
+        computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+        // post the new computer choice to the console for testing
+        console.log(computerChoice);
+        document.getElementById('guesses-so-far').innerHTML = "" ; 
+        document.getElementById('game-alert').innerHTML = "<span style='color: green;'>You won that round</span>";
+    } 
+
+    else {
         guesses--;
+
     }
+
+// if the user user loses, increase losses by 1, retrigger another guess by the computer, and cler the guesses so far field
 
     if(guesses === 0){
         losses++;
-
+        guesses = 10;
+        computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+        // post the new computer choice to the console for testing
+        console.log(computerChoice);
+        document.getElementById('guesses-so-far').innerHTML = "" ; 
+        document.getElementById('game-alert').innerHTML = "<span style='color: red;'>You lost that round</span>";
 
     }  
 
-    document.getElementById('wins').innerHTML = "Wins: " + wins;
-    document.getElementById('losses').innerHTML = "losses: " + losses;
-    document.getElementById('guesses').innerHTML = "Guesses left: " + guesses;
-    document.getElementById('guesses-so-far').innerHTML="Guesses so far: " + userGuess;
-   }
+// insert the score, guesses remaing, and guess so far into the HTML
+    document.getElementById('wins').innerHTML = wins;
+    document.getElementById('losses').innerHTML = losses;
+    document.getElementById('guesses').innerHTML =  guesses;
+    document.getElementById('guesses-so-far').innerHTML += " " + userGuess + " ";
+}
+ 
